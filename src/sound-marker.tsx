19f25b9817568a723723a990b27862e53eb13b48
saradeu <saradeu@cs.washington.edu>
2026-01-01 20:23:18 -0800

    added soundwalks to map as points for now

--- src/sound-marker.tsx
+++ src/sound-marker.tsx
@@ -4,10 +4,12 @@ import {AdvancedMarker, Pin} from '@vis.gl/react-google-maps';
 
 import {Sound} from './Sounds';
 
-export type Geometry = {
-  coordinates: string;
-  type: string;
-};
+// export type Geometry = {
+//   coordinates: string;
+//   type: string;
+// };
+
+export type Coordinate = [number, number];
 
 export type SoundMarkerProps = {
   sound: Sound;
@@ -28,30 +30,52 @@ export const SoundMarker = (props: SoundMarkerProps) => {
     [setMarkerRef, sound.key]
   );
 
-  // clunky filtering for now since I haven't dealt with soundwalks
-  const point = (sound.geometry.type == "Point") ? getLatLng(sound.geometry) : null;
+  // clunky for now since I haven't fully dealt with soundwalks yet
+  let point: { lat: number; lng: number };
+
+  if (sound.geometry.type == "LineString") {
+    const { line } = parseLineString(sound.geometry.coordinates);
+    const [lat, lng] = line[0];
+    point = { lat, lng };
+  } else {
+    point = getLatLng(sound.geometry.coordinates);
+  }
+
+  // const point = (sound.geometry.type == "Point") ? getLatLng(sound.geometry.coordinates) : parseLineString(sound.geometry.coordinates);
 
   // blue marker for points, purple markers for soundwalks
   const markerColor: string[] = (sound.geometry.type == "Point") ? ['#22ccff', '#1e89a1', '#0f677a'] : ['#C422FF', '#340047', '#560075'];
 
   return (
-    <>
-    {(sound.geometry.type == "Point") &&
-    (<AdvancedMarker position={point} ref={ref} onClick={handleClick}>
-    <Pin
-      background={markerColor[0]}
-      borderColor={markerColor[1]}
-      glyphColor={markerColor[2]}/>
-    </AdvancedMarker>)}
-    </>
+    <AdvancedMarker position={point} ref={ref} onClick={handleClick}>
+      <Pin
+        background={markerColor[0]}
+        borderColor={markerColor[1]}
+        glyphColor={markerColor[2]}/>
+    </AdvancedMarker>
   )
 }
 
-function getLatLng(geometry: Geometry): {
+function parseLineString(coordinates:string): {
+  line: Coordinate[]
+} {
+  const cleaned = coordinates.trim();
+
+  const coords = JSON.parse(cleaned) as [number, number][];
+  const flipped = coords.map(
+    ([a, b]) => [b, a] as Coordinate
+  );
+
+  return { line: flipped };
+}
+
+function getLatLng(coordinates: string): {
   lat: number;
   lng: number;
 } {
-  const coords = JSON.parse(geometry.coordinates) as [number, number];
+  const cleaned = coordinates.trim();
+
+  const coords = JSON.parse(cleaned) as [number, number];
 
   const [lng, lat] = coords;
 
